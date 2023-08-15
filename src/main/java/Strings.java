d5da3c7cc51eeb158727d4183fce7a32d21d21b7
Rick Morales <rick.morales7693@gmail.com>
2023-08-15 15:42:37 -0500

    added a class name

---
+++ src/main/java/Strings.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a input: ");
+        String input = scanner.nextLine();
+    }
+
+}
